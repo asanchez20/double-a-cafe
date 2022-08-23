c57777617accd4ef29e7552b314786286f1816a0
Aden Addis <adenwonde56@gmail.com>
2022-08-23 14:24:00 -0400

    added components and basic navbar

---
+++ double-a-cafe/src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function NavBar() {
+    return(
+        <>
+            <ul className='navbar'>
+                <li>Home</li>
+                <li>Menu</li>
+                <li>Shop Our Beans</li>
+            </ul> 
+        </>
+    )
+}
+
+export default NavBar;
